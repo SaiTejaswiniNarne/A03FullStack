b2d9b34fbfda80f5c7aeb8adb78a2f513f230c2e
Sai Tejaswini <S533726@nwmissouri.edu>
2018-10-28 20:20:02 -0500

    added

--- public/test/test.js
+++ public/test/test.js
@@ -3,10 +3,8 @@ QUnit.test("Here's a test that should always pass", function (assert) {
 });
 
 QUnit.test('Testing calculateSeats function with several sets of inputs', function (assert) {
-  assert.equal(App.calculateSeats(2, 3), 6, 'calculate (5, 5) returns 25 - Bonus1');
-  assert.equal(App.calculateSeats(500, 500), 0, 'calculate (500, 500) return 0 - Bonus2');
-  assert.equal(App.calculateSeats(-5, -5), 0, 'calculate (-5, -5) return 0 - Bonus3');
-  assert.equal(App.calculateSeats(3.4, 5.7), 19.38, 'calculate (3.5, 5.7) return 0 - Bonus4');
-  assert.equal(App.calculateSeats('a', 'b'), 0, 'bonus5- calculate(3) return 0' );
-  assert.equal(App.calculateSeats(' ',' '), 0, 'calculate (3) - Bonus6');
+  assert.strictEqual(),new alert("You Lose!"),'Suessfully resulted';
+  assert.strictEqual(),new alert("You Lose!"),'Suessfully resulted';
+  assert.strictEqual(),new alert("You Lose!"),'Suessfully resulted';
+  assert.strictEqual(),new alert("You Lose!"),'Suessfully resulted';
 });
